de6a799ff2291d4ae1925a0a947917b29d2a00b0
Wallington <wallingtonbandeira31@gmail.com>
2020-05-25 23:57:27 +0200

    añadidos comentarios

--- MainScript.ino
+++ MainScript.ino
@@ -1,31 +1,39 @@
-const int VALOR_EN_AIRE = 620;  //valor maximo que da el sensor en aire
-const int VALOR_EN_AGUA = 310;  //valor maximo que da el sensor totalmente submergido
+
+/**Para obtener el valor de VALOR EN AIRE y VALOR EN AGUA es necesario hacer pruebas 
+ * antes, cada sensor puede dar valores distintos
+ */
+const int VALOR_EN_AIRE = 620; //valor maximo que da el sensor en aire
+const int VALOR_EN_AGUA = 310; //valor maximo que da el sensor totalmente submergido
 const int PIN_SENSOR_HUMEDAD = A0;
 
 int valorSensorHumedad = 0;
 int porcentajeValorSensorHumedad = 0;
 
-void setup() {
-    Serial.begin(9600);     //abre el puerto serial
+void setup()
+{
+    Serial.begin(9600); //abre el puerto serial
 }
 
 //En estas funcion es donde se estara ejecutando el codigo de la placa en bucle
-void loop() {
-    
+void loop()
+{
 }
 
 //Actualiza las variables con una lectura del sensor
-void leerHumedadSuelo() {
+void leerHumedadSuelo()
+{
     valorSensorHumedad = analogRead(PIN_SENSOR_HUMEDAD);
     porcentajeValorSensorHumedad = map(valorSensorHumedad, VALOR_EN_AIRE, VALOR_EN_AGUA, 0, 100);
 }
 
-int getValorSensorHumedad() {
+int getValorSensorHumedad()
+{
     leerHumedadSuelo();
     return valorSensorHumedad;
 }
 
-int getPorcentajeHumedad() {
+int getPorcentajeHumedad()
+{
     leerHumedadSuelo();
     return porcentajeValorSensorHumedad;
 }
