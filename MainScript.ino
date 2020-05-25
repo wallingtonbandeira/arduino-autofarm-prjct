392415a3e177a2cee9e48ba52e18df2f20884a05
Wallington <wallingtonbandeira31@gmail.com>
2020-05-25 23:49:21 +0200

    funciones sensor de humedad añadidas

--- MainScript.ino
+++ MainScript.ino
@@ -1,5 +1,6 @@
 const int VALOR_EN_AIRE = 620;  //valor maximo que da el sensor en aire
 const int VALOR_EN_AGUA = 310;  //valor maximo que da el sensor totalmente submergido
+const int PIN_SENSOR_HUMEDAD = A0;
 
 int valorSensorHumedad = 0;
 int porcentajeValorSensorHumedad = 0;
@@ -10,9 +11,21 @@ void setup() {
 
 //En estas funcion es donde se estara ejecutando el codigo de la placa en bucle
 void loop() {
-
+    
 }
 
-int leerHumedadSuelo() {
-    
+//Actualiza las variables con una lectura del sensor
+void leerHumedadSuelo() {
+    valorSensorHumedad = analogRead(PIN_SENSOR_HUMEDAD);
+    porcentajeValorSensorHumedad = map(valorSensorHumedad, VALOR_EN_AIRE, VALOR_EN_AGUA, 0, 100);
+}
+
+int getValorSensorHumedad() {
+    leerHumedadSuelo();
+    return valorSensorHumedad;
+}
+
+int getPorcentajeHumedad() {
+    leerHumedadSuelo();
+    return porcentajeValorSensorHumedad;
 }
